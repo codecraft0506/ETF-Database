5b94070c8199afe9cb54564eed2a7d103bac6650
Jianrong0830 <ryanchen0830b@gmail.com>
2024-12-07 21:43:06 +0800

    1207

--- services/engines/yahoo.py
+++ services/engines/yahoo.py
@@ -7,6 +7,8 @@ def get_yahoo_data(symbol):
         dividend_recovery_days = "-"
         dividend_amount = "-"
         dividend_yield = "-"
+        one_year_dividend_amount = "-"
+        one_year_dividend_yield = "-"
 
         dividend_url = f"https://tw.stock.yahoo.com/quote/{symbol}.TWO/dividend"
         dividend_response = requests.get(dividend_url, timeout=10)
@@ -19,29 +21,44 @@ def get_yahoo_data(symbol):
         while (len(dividend_recovery_days) < 4) and (i < 12):
             i += 1
             try:
-                dividend_recovery_day = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[11]/text()')
-                dividend_recovery_day = dividend_recovery_day[0].strip()
+                dividend_recovery_day = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[11]/text()')[0].strip()
                 if dividend_recovery_day and dividend_recovery_day != '' and dividend_recovery_day != ' ':
                     dividend_recovery_days.append(dividend_recovery_day)
                     
                     if len(dividend_recovery_days) == 1:
                         try:
-                            dividend_amount = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[3]/span/text()')
-                            dividend_amount = dividend_amount[0].strip() if dividend_amount else "-"
+                            dividend_amount = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[3]/span/text()')[0].strip()
                         except Exception as e:
                             dividend_amount = "Error"
-            
+
                         try:
-                            dividend_yield = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[5]/span/text()')
-                            dividend_yield = dividend_yield[0].strip() if dividend_yield else "-"
+                            dividend_yield = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[5]/span/text()')[0].strip()
                         except Exception as e:
                             dividend_yield = "Error"
-        
+                else:
+                    if (one_year_dividend_amount == "-" and one_year_dividend_yield == "-") or (one_year_dividend_amount == "Error" and one_year_dividend_yield == "Error"):
+                        try:
+                            one_year_dividend_amount = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[3]/span/text()')[0].strip()
+                            one_year_dividend_yield = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[5]/span/text()')[0].strip()
+                        except Exception as e:
+                            one_year_dividend_amount = "Error"
+                            one_year_dividend_yield = "Error"
             except Exception as e:
+                if (one_year_dividend_amount == "-" and one_year_dividend_yield == "-") or (one_year_dividend_amount == "Error" and one_year_dividend_yield == "Error"):
+                    try:
+                        one_year_dividend_amount = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[3]/span/text()')[0].strip()
+                        one_year_dividend_yield = dividend_tree.xpath(f'//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[3]/div[2]/div/div/ul/li[{i}]/div/div[5]/span/text()')[0].strip()
+                    except Exception as e:
+                        one_year_dividend_amount = "Error"
+                        one_year_dividend_yield = "Error"
                 continue
         
         if len(dividend_recovery_days) == 0:
             dividend_recovery_days = "-"
+            dividend_amount = "-"
+            dividend_yield = "-"
+            one_year_dividend_amount = "-"
+            one_year_dividend_yield = "-"
         elif len(dividend_recovery_days) < 4:
             dividend_recovery_days += "(Error: 未滿4次)"
         else:
@@ -51,6 +68,8 @@ def get_yahoo_data(symbol):
         dividend_recovery_days = "Error"
         dividend_amount = "Error"
         dividend_yield = "Error"
+        one_year_dividend_amount = "Error"
+        one_year_dividend_yield = "Error"
 
     # profile
     try:
@@ -125,8 +144,27 @@ def get_yahoo_data(symbol):
         "除息日": ex_dividend_date,
         "年化報酬率": annual_return,
         "年初至今總報酬率": return_till_today,
-        "一個月總報酬率": one_month_return
+        "一個月總報酬率": one_month_return,
+        "近四季累積配息": one_year_dividend_amount,
+        "近四季殖利率": one_year_dividend_yield
     }
 
 if __name__ == "__main__":
-    print(get_yahoo_data("00662"))
+    symbols = [
+        '0052', '0053', '0055', '00662', '00671R', '00678', '00712', '00714', '00728', '00735', 
+        '00737', '00757', '00762', '00770', '00830', '00851', '00861', '00875', '00876', '00877', 
+        '00881', '00886', '00887', '00891', '00892', '00895', '00896', '00897', '00898', '00899', 
+        '00901', '00902', '00903', '00904', '00908', '00911', '00679B', '00687B', '00694B', '00695B', 
+        '00696B', '00697B', '00719B', '00764B', '00768B', '00779B', '00795B', '00847B', '00856B', 
+        '00857B', '00859B', '00864B', '00865B', '00931B', '00720B', '00722B', '00723B', '00724B', 
+        '00725B', '00734B', '00740B', '00746B', '00749B', '00750B', '00751B', '00754B', '00755B', 
+        '00759B', '00761B', '00772B', '00773B', '00775B', '00777B', '00778B', '00780B', '00781B', 
+        '00782B', '00785B', '00786B', '00787B', '00788B', '00789B', '00790B', '00791B', '00792B', 
+        '00793B', '00799B', '00834B', '00836B', '00840B', '00841B', '00842B', '00844B', '00845B', 
+        '00846B', '00853B', '00860B', '00862B', '00863B', '00867B', '00883B', '00890B', '00937B', 
+        '00942B', '00711B', '00718B', '00726B', '00756B', '00760B', '00784B', '00794B', '00848B', 
+        '00849B', '00870B', '00884B', '00635U', '00642U', '00673R', '00674R', '00693U', '00708L', 
+        '00715L', '00738U', '00763U', '00682U', '00683L', '00684R', '00706L', '00707R'
+    ]
+    for symbol in symbols:
+        print(symbol, get_yahoo_data(symbol))
